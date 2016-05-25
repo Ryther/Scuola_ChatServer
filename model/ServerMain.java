15b3937cbbe656d08a8c8d30321498b03320572a
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-05-25 16:25:51 +0200

    Creati package per migliorare la gestione del progetto

--- model/ServerMain.java
+++ model/ServerMain.java
@@ -1,3 +1,8 @@
+package model;
+
+import net.Talker;
+import data.Consts;
+import data.ChatMessage;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import net.SocketHandler;
