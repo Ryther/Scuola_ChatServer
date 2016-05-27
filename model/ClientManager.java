b43fa1cb3972b6b9120210f6f735689a8b2a6788
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-05-27 11:15:23 +0200

    [ClientManager.java]Prima implementazione per multi-client

--- model/ClientManager.java
+++ model/ClientManager.java
@@ -1,4 +1,14 @@
-package model.clientManager;
+package model;
+
+import chatUtils.data.Chat;
+import chatUtils.data.ChatMessage;
+import chatUtils.data.Consts;
+import chatUtils.data.UserData;
+import chatUtils.net.Talker;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import utils.net.StreamHandler;
 
 /**
  *
@@ -6,8 +16,24 @@ package model.clientManager;
  */
 public class ClientManager implements Runnable {
 
+    private final StreamHandler streamHandler;
+    private final Set<Chat> chats;
+    private UserData userData;
+
+    public ClientManager(StreamHandler streamHandler, Set<Chat> chats) {
+        
+        this.streamHandler = streamHandler;
+        this.chats = chats;
+    }
     
     public void run() {
         
+        this.streamHandler.init();
+        userData = (UserData) this.streamHandler.pullFromStream();
+        ExecutorService threadPool = Executors.newFixedThreadPool(Consts.TALKER_THREADS);
+        Runnable writingThread = new Talker(streamHandler, new ChatMessage(this.userData.getUserName()));
+        Runnable readingThread = new Talker(streamHandler);
+        threadPool.submit(writingThread);
+        threadPool.submit(readingThread);
     }
 }
