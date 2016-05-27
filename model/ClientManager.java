40e16686a87041928cc24fa1180f1b3ba65ec2dd
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-05-27 21:10:34 +0200

    Creata logica per invio messaggi a clients multipli

--- model/ClientManager.java
+++ model/ClientManager.java
@@ -1,13 +1,12 @@
 package model;
 
-import chatUtils.data.Chat;
 import chatUtils.data.ChatMessage;
 import chatUtils.data.Consts;
 import chatUtils.data.UserData;
 import chatUtils.net.Talker;
-import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import model.data.ServerData;
 import utils.net.StreamHandler;
 
 /**
@@ -17,19 +16,28 @@ import utils.net.StreamHandler;
 public class ClientManager implements Runnable {
 
     private final StreamHandler streamHandler;
-    private final Set<Chat> chats;
-    private UserData userData;
+    private final ServerData serverData;
 
-    public ClientManager(StreamHandler streamHandler, Set<Chat> chats) {
+    public ClientManager(StreamHandler streamHandler, ServerData serverData) {
         
         this.streamHandler = streamHandler;
-        this.chats = chats;
+        this.serverData = serverData;
     }
     
     public void run() {
         
         this.streamHandler.init();
+        
+        UserData userData;
         userData = (UserData) this.streamHandler.pullFromStream();
+        serverData.addUser(userData);
+        serverData.addUserToChat(userData.getUserName(), "Test");
+        
+        while (true) {
+            
+            ChatMessage chatMessage = (ChatMessage) this.streamHandler.pullFromStream();
+            //for (UserData chatUsers:this.serverData.getChat(chatMessage.getChatName()).getUsers().entrySet())
+        }
         ExecutorService threadPool = Executors.newFixedThreadPool(Consts.TALKER_THREADS);
         Runnable writingThread = new Talker(streamHandler, new ChatMessage(this.userData.getUserName()));
         Runnable readingThread = new Talker(streamHandler);
--- model/ServerMain.java
+++ model/ServerMain.java
@@ -2,10 +2,9 @@ package model;
 
 import chatUtils.data.Chat;
 import chatUtils.data.Consts;
-import java.util.Set;
-import java.util.TreeSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import model.data.ServerData;
 import utils.net.SocketHandler;
 import utils.net.StreamHandler;
 
@@ -19,16 +18,17 @@ public class ServerMain {
     
     public static void main(String[] args) {
         
+        ServerData serverData = new ServerData();
+        serverData.addChat(new Chat("Test"));
         ExecutorService threadPool = Executors.newFixedThreadPool(Consts.CLIENT_THREADS);
         
         socketHandler = new SocketHandler(Consts.PORT);
-        Set<Chat> chats = new TreeSet();
         
         while (true) {
             socketHandler.accept();
 
             Runnable clientManager = new ClientManager(
-                    new StreamHandler(socketHandler.getSocket()), chats);
+                    new StreamHandler(socketHandler.getSocket()), serverData);
 
             threadPool.submit(clientManager);
         }
